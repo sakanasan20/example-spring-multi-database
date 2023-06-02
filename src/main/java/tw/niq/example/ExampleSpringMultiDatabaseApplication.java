96fff1cf5fd03b77324d9e452e3a4d4b221a4d48
Nick <nick.chen@cmp.com.tw>
2023-06-02 09:22:09 +0800

    Initial commit

---
+++ src/main/java/tw/niq/example/ExampleSpringMultiDatabaseApplication.java
@@ -0,0 +1,13 @@
+package tw.niq.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExampleSpringMultiDatabaseApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExampleSpringMultiDatabaseApplication.class, args);
+	}
+
+}
